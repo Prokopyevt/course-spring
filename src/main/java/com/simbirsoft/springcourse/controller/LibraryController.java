47efa01775f8dde38d55b5ba612cec961c2f401d
Prokopyev Tikhon <johndoe@example.com>
2020-11-24 21:54:47 +0400

    Переправленное Домашнее задание 3 урока. 1.Переименована  таблица из "userdata" в "users" 2.Добавлен @repository в userRepository 3.Добавлены permissions 4. Удалены неиспользуемые импорты

--- src/main/java/com/simbirsoft/springcourse/controller/LibraryController.java
+++ src/main/java/com/simbirsoft/springcourse/controller/LibraryController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.util.StringUtils.isEmpty;
+
 
 @RestController
 @RequestMapping("/api/v1/library")
